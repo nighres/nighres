64d6b4a952ee1875ca1e815818dfeec7eb27d3e3
juhuntenburg <ju.huntenburg@gmail.com>
2017-07-21 14:03:44 +0200

    moving topology_lut_dir

--- cbstoolspython/global_settings.py
+++ cbstoolspython/global_settings.py
@@ -1,5 +1,6 @@
 import os
 
-TOPOLOGY_LUT_DIR = os.path.join(os.getcwd(), 'topology_lut/')
-ATLAS_DIR = os.path.join(os.getcwd(), 'atlases/brain-segmentation-prior3.0/')
-DEFAULT_ATLAS = os.path.join(ATLAS_DIR, 'brain-atlas-3.0.3.txt')
+ATLAS_DIR = os.path.join(os.getcwd(), 'atlases/')
+TOPOLOGY_LUT_DIR = os.path.join(ATLAS_DIR, 'topology_lut/')
+DEFAULT_ATLAS = os.path.join(ATLAS_DIR,
+                        'brain-segmentation-prior3.0/brain-atlas-3.0.3.txt')
