716f58a00105389af668b626a4e5b43c34ffbe47
juhuntenburg <ju.huntenburg@gmail.com>
2017-07-26 12:39:20 +0200

    renaming python wrapper module to nires

--- nires/global_settings.py
+++ nires/global_settings.py
@@ -1,6 +1,8 @@
 import os
 
-ATLAS_DIR = os.path.join(os.getcwd(), 'atlases/')
+__dir__ = os.path.abspath(os.path.dirname(__file__))
+
+ATLAS_DIR = os.path.join(__dir__, 'atlases/')
 TOPOLOGY_LUT_DIR = os.path.join(ATLAS_DIR, 'topology_lut/')
 DEFAULT_ATLAS = os.path.join(ATLAS_DIR,
                         'brain-segmentation-prior3.0/brain-atlas-3.0.3.txt')
