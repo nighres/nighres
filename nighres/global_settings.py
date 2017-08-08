cbf49c6f24f931a3c53afa63f3f01999a5434110
juhuntenburg <ju.huntenburg@gmail.com>
2017-08-08 15:56:31 +0200

    cleaner path join in global settings

--- nighres/global_settings.py
+++ nighres/global_settings.py
@@ -2,7 +2,7 @@ import os
 
 __dir__ = os.path.abspath(os.path.dirname(__file__))
 
-ATLAS_DIR = os.path.join(__dir__, 'atlases/')
-TOPOLOGY_LUT_DIR = os.path.join(ATLAS_DIR, 'topology_lut/')
-DEFAULT_ATLAS = os.path.join(ATLAS_DIR,
-                        'brain-segmentation-prior3.0/brain-atlas-3.0.3.txt')
+ATLAS_DIR = os.path.join(__dir__, 'atlases')
+TOPOLOGY_LUT_DIR = os.path.join(ATLAS_DIR, 'topology_lut')
+DEFAULT_ATLAS = os.path.join(ATLAS_DIR, 'brain-segmentation-prior3.0',
+                             'brain-atlas-3.0.3.txt')
