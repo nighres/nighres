b8a947da87b16e88921091bd1600e331e30e0515
juhuntenburg <ju.huntenburg@gmail.com>
2017-08-16 17:50:13 +0200

    trying to fix testpypi upload and figure out cpu architecture

--- build.sh
+++ build.sh
@@ -155,6 +155,9 @@ cp -rv build/cbstools/ ../
 find build/ -type f | grep '.so$' | head -n 1 | xargs -I '{}' -- cp '{}' ../cbstools/_cbstools.so
 cd ..
 
+uname -a
+lscpu
+
 # Make the python wheel
 # TODO get platform and python version from system
 (
