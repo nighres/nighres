6c8a850e082d22ae2796680055eec5ec7eaf80f3
juhuntenburg <ju.huntenburg@gmail.com>
2017-08-17 15:26:14 +0200

    change version to see if testpypi works better with smaller package

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open('README.rst') as f:
 
 setup(
     name='nighres',
-    version='0.2.2',
+    version='0.2.3',
     description='Processing tools for high-resolution neuroimaging',
     long_description=long_description,
     url='https://nighres.readthedocs.io/',
