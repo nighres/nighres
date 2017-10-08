d1fd2f30aa83275f35f626e5425fc1f0ef2c491f
juhuntenburg <ju.huntenburg@gmail.com>
2017-10-08 21:13:52 +0200

    fixing location of unpacked dir in make_dist

--- travis/make_dist.sh
+++ travis/make_dist.sh
@@ -7,7 +7,7 @@ python setup.py sdist
 # make a new folder to test the installation
 mkdir "$HOME"/test_install
 tar -xf dist/nighres* -C "$HOME"/test_install
-cd "$HOME"/test_install
+cd "$HOME"/test_install/nighres*
 ./build.sh
 pip install .
 cd "$HOME"
