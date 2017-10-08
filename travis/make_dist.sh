126bc337f62b2dc84a3d482703eeda58384ff98e
juhuntenburg <ju.huntenburg@gmail.com>
2017-10-08 21:08:03 +0200

    fix tar command in make_dist

--- travis/make_dist.sh
+++ travis/make_dist.sh
@@ -6,7 +6,7 @@ python setup.py sdist
 
 # make a new folder to test the installation
 mkdir "$HOME"/test_install
-tar -xf dist/nighres* "$HOME"/test_install
+tar -xf dist/nighres* -C "$HOME"/test_install
 cd "$HOME"/test_install
 ./build.sh
 pip install .
