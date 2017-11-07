40b0df4aae1ff20346469eb5d8746dec285e45d3
juhuntenburg <ju.huntenburg@gmail.com>
2017-11-07 23:59:01 +0100

    mock import for _cbstools.so also

--- doc/conf.py
+++ doc/conf.py
@@ -118,7 +118,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['libjvm.so']
+MOCK_MODULES = ['libjvm.so', '_cbstools.so']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
