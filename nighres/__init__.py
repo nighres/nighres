935ba4ff59a0b3eccaf3eaa9376d8fde375109a3
piloubazin <piloubazin@gmail.com>
2017-11-22 15:37:55 +0100

    Update __init__.py

--- nighres/__init__.py
+++ nighres/__init__.py
@@ -3,8 +3,9 @@ import brain
 import cortex
 import data
 import laminar
+import filtering
 import registration
 import surface
 from global_settings import ATLAS_DIR, TOPOLOGY_LUT_DIR, DEFAULT_ATLAS
 
-__all__ = ['io', 'brain', 'cortex', 'data', 'laminar', 'registration', 'surface', '__version__']
+__all__ = ['io', 'brain', 'cortex', 'data', 'laminar', 'filtering', 'registration', 'surface', '__version__']
