50eed632314870cc62c75f0d0d81491d5f52ac6f
Pilou Bazin <piloubazin@gmail.com>
2018-01-12 19:45:02 +0100

    cell seg trails

--- nighres/__init__.py
+++ nighres/__init__.py
@@ -3,9 +3,10 @@ import brain
 import cortex
 import data
 import laminar
+import segmentation
 import surface
 import filtering
 from global_settings import ATLAS_DIR, TOPOLOGY_LUT_DIR, DEFAULT_ATLAS
 
-__all__ = ['io', 'brain', 'laminar', 'surface',
+__all__ = ['io', 'brain', 'cortex', 'laminar', 'segmentation', 'surface',
            'data', 'filtering', '__version__']
