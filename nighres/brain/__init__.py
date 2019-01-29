a855cab43bf75bb1d6c00cb53b3cdefc7e34297b
Pilou Bazin <piloubazin@gmail.com>
2019-01-29 20:15:20 +0100

    new module for generic intensity-basedskull stripping

--- nighres/brain/__init__.py
+++ nighres/brain/__init__.py
@@ -1,5 +1,6 @@
 from nighres.brain.extract_brain_region import extract_brain_region
 from nighres.brain.filter_stacking import filter_stacking
+from nighres.brain.intensity_based_skullstripping import intensity_based_skullstripping
 from nighres.brain.mgdm_segmentation import mgdm_segmentation
 from nighres.brain.mp2rage_dura_estimation import mp2rage_dura_estimation
 from nighres.brain.mp2rage_skullstripping import mp2rage_skullstripping
