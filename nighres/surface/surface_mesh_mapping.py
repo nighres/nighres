5f99230ba2d5d8bdaef8d0f746543526c6a3036f
Pilou Bazin <piloubazin@gmail.com>
2019-03-01 17:29:48 +0100

    mixed up i/o functions

--- nighres/surface/surface_mesh_mapping.py
+++ nighres/surface/surface_mesh_mapping.py
@@ -3,7 +3,7 @@ import sys
 import numpy as np
 import nibabel as nb
 import nighresjava
-from ..io import load_volume, save_volume, load_mesh_geometry, save_mesh_geometry
+from ..io import load_volume, save_volume, load_mesh_geometry, save_mesh
 from ..utils import _output_dir_4saving, _fname_4saving,_check_available_memory
 
 
