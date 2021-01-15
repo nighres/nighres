cf65305ad940ddd3ac5339ee548a065bf1728485
Pierre-Louis Bazin <pilou.bazin@uva.nl>
2021-01-15 08:49:38 +0100

    merging

--- build_local_nighresjava.sh
+++ build_local_nighresjava.sh
@@ -4,6 +4,7 @@
 #
 ## SETUP
 #
+echo "Setup..."
 
 set -euo pipefail
 unset CDPATH; cd "$( dirname "${BASH_SOURCE[0]}" )"; cd "$(pwd -P)"
@@ -20,16 +21,16 @@ nighres_local="/home/pilou/Code/github/nighres"
 hash wget tar javac jar python3 2>/dev/null || fatal "This script needs the following commands available: wget tar javac jar python3"
 
 # Check for setuptools and wheels
-pip_modules=$(python3 -m pip list | tr -s ' ' | cut -f 1 -d ' ')
+pip_modules=$(python3 -m pip list --format=columns | tr -s ' ' | cut -f 1 -d ' ')
 echo "${pip_modules}" | grep setuptools > /dev/null || fatal 'This script requires setuptools.\nInstall with `pip install --upgrade setuptools`'
 echo "${pip_modules}" | grep wheel > /dev/null || fatal 'This script requires wheel.\nInstall with `pip install --upgrade wheel`'
 
 # echo "Before detection: $JAVA_HOME"
 
 # Set the JAVA_HOME variable if it is not set
-detected_home=$(java -XshowSettings:properties -version 2>&1 | tr -d ' '| grep java.home | cut -f 2 -d '=')
+detected_home=$(javac -XshowSettings:properties -version 2>&1 | tr -d ' '| grep java.home | cut -f 2 -d '=')
 export JAVA_HOME=${JAVA_HOME:-"$detected_home"}
-# echo "After detection: $JAVA_HOME"
+echo "After detection: $JAVA_HOME"
 
 # Check that JCC is installed
 echo "${pip_modules}" | grep JCC > /dev/null || fatal 'This script requires JCC.\nInstall with `apt-get install jcc` or equivalent and `pip install jcc`'
