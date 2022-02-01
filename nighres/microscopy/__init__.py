48830ea0efb104b7b88cec99d6a7b7901dddc6f1
Pierre-Louis Bazin <piloubazin@gmail.com>
2022-02-01 15:38:37 +0100

    new module for line analysis

--- nighres/microscopy/__init__.py
+++ nighres/microscopy/__init__.py
@@ -2,3 +2,5 @@ from nighres.microscopy.mgdm_cells import mgdm_cells
 from nighres.microscopy.stack_intensity_regularisation import stack_intensity_regularisation
 from nighres.microscopy.stack_intensity_mapping import stack_intensity_mapping
 from nighres.microscopy.directional_line_clustering import directional_line_clustering
+from nighres.microscopy.shape_tensor_matching import shape_tensor_matching
+from nighres.microscopy.linear_fiber_mapping import linear_fiber_mapping
