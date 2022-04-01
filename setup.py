6f7610772560adef761c063adbcd1c965af70009
Remi Gau <remi_gau@hotmail.com>
2022-04-01 10:21:11 +0200

    add dipy as dependency of nighres

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     keywords='MRI high-resolution laminar subcortex',
     packages=find_packages(),
     include_package_data=True,
-    install_requires=['numpy==1.22.3', 'nibabel==3.2.2', 'psutil==5.9.0', 'antspyx==0.3.2'],
+    install_requires=['numpy==1.22.3', 'nibabel==3.2.2', 'psutil==5.9.0', 'antspyx==0.3.2', 'dipy==1.5.0'],
     python_requires='>=3'
 )
